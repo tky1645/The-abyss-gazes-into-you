a26c7fc0bee959bcbf7d13b4371d658f4a4bba4c
tky1645 <45508735+tky1645@users.noreply.github.com>
2020-04-11 12:11:11 +0900

    エクセルの読み込みテストを追加

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,1 +1,18 @@
 import numpy as np
+import openpyxl as op
+
+
+#シート読み込み
+book = op.load_workbook('test.xlsx')
+
+#シート数と名前を取得
+print('--------------------------------')
+print(len(book.sheetnames))
+print('--------------------------------')
+for name in book.get_sheet_names():
+    print(name)
+
+#セル値の取得
+activeSheet = book.active
+print(activeSheet.cell(column=1,row=1).value)
+
